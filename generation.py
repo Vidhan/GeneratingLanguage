cc9ca85502d08961ff7a9c41103fa817caa41cba
Vidhan Agarwal <whizvids@gmail.com>
2018-03-24 22:17:04 -0400

    Update generation.py

--- generation.py
+++ generation.py
@@ -4,13 +4,13 @@ import os
 
 
 def generation(inp, forward):
-    '''
-    Generate a sequence of words given a starting sequence.
-    Load your model before generating words.
-    :param inp: Initial sequence of words (batch size, length)
-    :param forward: number of additional words to generate
-    :return: generated words (batch size, forward)
-    '''
+    
+    # Generate a sequence of words given a starting sequence.
+    # Load your model before generating words.
+    # :param inp: Initial sequence of words (batch size, length)
+    # :param forward: number of additional words to generate
+    # :return: generated words (batch size, forward)
+    
     args = args_packer()
     model = MyLSTMModel(args)
     filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model.pt')
