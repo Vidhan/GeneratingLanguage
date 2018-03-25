bce3b751e696ebff26f71c3c845cd7247d258f69
Vidhan Agarwal <whizvids@gmail.com>
2018-03-24 22:14:49 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -4,13 +4,13 @@ import os
 
 
 def prediction(inp):
-    '''
-    Input is a text sequences. Produce scores for the next word in the sequence.
-    Scores should be raw logits not post-softmax activations.
-    Load your model before generating predictions.
-    :param inp: array of words (batch size, sequence length) [0-labels]
-    :return: array of scores for the next word in each sequence (batch size, labels)
-    '''
+    
+    # Input is a text sequences. Produce scores for the next word in the sequence.
+    # Scores should be raw logits not post-softmax activations.
+    # Load your model before generating predictions.
+    # :param inp: array of words (batch size, sequence length) [0-labels]
+    # :return: array of scores for the next word in each sequence (batch size, labels)
+    
     args = args_packer()
     model = MyLSTMModel(args)
     filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model.pt')
